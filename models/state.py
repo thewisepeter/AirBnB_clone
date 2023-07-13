99e20e7ab99efb424348b96a63a21507ce84db4d
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-07-13 14:15:24 +0300

    Fixed state.py

--- models/state.py
+++ models/state.py
@@ -1,14 +1,5 @@
 from models.base_model import BaseModel
 
-
 class State(BaseModel):
-    """
-    A subclass of BaseModel representing a state.
-    
-    Public class attribute:
-        name: (str) the name of the state
-    """
-    
-    def __init__(self, name=""):
-        super().__init__()
-        self.name = name
+    """State class that inherits from BaseModel"""
+    name = ""
