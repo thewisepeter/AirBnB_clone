dd6b73a8dbac0197ed0c592e98c53fdb3abc518b
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-07-13 14:15:51 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,16 +1,6 @@
 from models.base_model import BaseModel
 
-
 class City(BaseModel):
-    """
-    A subclass of BaseModel representing a city.
-    
-    Public class attributes:
-        state_id: (str) the ID of the state to which the city belongs
-        name: (str) the name of the city
-    """
-    
-    def __init__(self, state_id="", name=""):
-        super().__init__()
-        self.state_id = state_id
-        self.name = name
+    """City class that inherits from BaseModel"""
+    state_id = ""
+    name = ""
