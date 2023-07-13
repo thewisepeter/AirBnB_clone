3d0a623e4719ec50e04c5ed41d1f72ec7dc465a2
Kiirya Arnold <32293740+arnoldkiirya1@users.noreply.github.com>
2023-07-13 14:16:16 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,5 @@
 from models.base_model import BaseModel
 
-
 class Amenity(BaseModel):
-    """
-    A subclass of BaseModel representing an amenity.
-    
-    Public class attribute:
-        name: (str) the name of the amenity
-    """
-    
-    def __init__(self, name=""):
-        super().__init__()
-        self.name = name
+    """Amenity class that inherits from BaseModel"""
+    name = ""
