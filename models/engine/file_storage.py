6500b0ab8117fc170470f262ccb061ab93c6e69e
Peter Ochan <rempeterwisdom@gmail.com>
2023-07-16 16:48:42 +0300

    encapsulation in filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,30 +31,30 @@ class FileStorage:
 
     def all(self):
         '''Return dictionary of <class>.<id> : object instance'''
-        return self.__objects
+        return type(self).__objects
 
     def new(self, obj):
         '''Set new __objects to existing dictionary of instances'''
         if obj:
             key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-            self.__objects[key] = obj
+            type(self).__objects[key] = obj
 
     def save(self):
         """Save/serialize obj dictionaries to json file"""
         obj_dict = {}
 
-        for key, obj in self.__objects.items():
+        for key, obj in type(self).__objects.items():
             obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
+        with open(type(self).__file_path, 'w', encoding="UTF-8") as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """Deserialize/convert obj dicts back to instances, if it exists"""
         try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
+            with open(type(self).__file_path, 'r', encoding="UTF-8") as f:
                 new_obj_dict = json.load(f)
                 for key, value in new_obj_dict.items():
                     obj = self.class_dict[value['__class__']](**value)
-                    self.__objects[key] = obj
+                    type(self).__objects[key] = obj
         except FileNotFoundError:
             pass
