d147819323a0c716842dad728814b845ad3cff66
Peter Ochan <rempeterwisdom@gmail.com>
2023-07-18 12:05:15 +0300

    added tests for console

--- tests/test_console.py
+++ tests/test_console.py
@@ -7,6 +7,13 @@ import sys
 from io import StringIO
 from unittest.mock import patch
 from console import HBNBCommand
+from models.base_model import BaseModel
+from models.user import User
+from models.review import Review
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
 
 
 class TestHBNBCommand(unittest.TestCase):
@@ -84,14 +91,6 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertFalse(HBNBCommand().onecmd("help help"))
             self.assertEqual(expected_output, output.getvalue().strip())
 
-    def test_help(self):
-        expected_output = "Documented commands (type help <topic>):\n" \
-                "========================================\n" \
-                "EOF  all  count  create  destroy  help  quit  show  update"
-        with patch("sys.stdout", new=StringIO()) as output:
-            self.assertFalse(HBNBCommand().onecmd("help"))
-            self.assertEqual(expected_output, output.getvalue().strip())
-    
     def test_quit(self):
         """
         Test quit command
@@ -228,6 +227,184 @@ class TestHBNBCommand(unittest.TestCase):
             self.assertFalse(HBNBCommand().onecmd("invalid_command"))
             self.assertEqual(f.getvalue(),
                              "*** Unknown syntax: invalid_command\n")
+    
+    def test_all_base_model(self):
+        # Create a few instances of BaseModel
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        instance3 = BaseModel()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all BaseModel")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance1.__str__(), output_str)
+            self.assertIn(instance2.__str__(), output_str)
+            self.assertIn(instance3.__str__(), output_str)
+    
+    def test_all_user(self):
+        # Create a few instances of User
+        instance1 = User()
+        instance2 = User()
+        instance3 = User()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all User")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance1.__str__(), output_str)
+            self.assertIn(instance2.__str__(), output_str)
+            self.assertIn(instance3.__str__(), output_str)
+
+    def test_all_with_updated_instance(self):
+        # Create an instance of BaseModel
+        instance = BaseModel()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all BaseModel")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+
+        # Update the instance
+        instance.name = "Updated Instance"
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all BaseModel")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+    
+    def test_all_review(self):
+        # Create a few instances of Review
+        instance1 = Review()
+        instance2 = Review()
+        instance3 = Review()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all Review")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance1.__str__(), output_str)
+            self.assertIn(instance2.__str__(), output_str)
+            self.assertIn(instance3.__str__(), output_str)
+
+    def test_all_with_updated_instance(self):
+        # Create an instance of Review
+        instance = Review()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all Review")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+
+        # Update the instance
+        instance.text = "Updated Review"
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all Review")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+
+    def test_all_state(self):
+        # Create a few instances of State
+        instance1 = State()
+        instance2 = State()
+        instance3 = State()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all State")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance1.__str__(), output_str)
+            self.assertIn(instance2.__str__(), output_str)
+            self.assertIn(instance3.__str__(), output_str)
+
+    def test_all_with_updated_instance(self):
+        # Create an instance of State
+        instance = State()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all State")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+
+        # Update the instance
+        instance.name = "Updated State"
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all State")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+
+    def test_all_city(self):
+        # Create a few instances of City
+        instance1 = City()
+        instance2 = City()
+        instance3 = City()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all City")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance1.__str__(), output_str)
+            self.assertIn(instance2.__str__(), output_str)
+            self.assertIn(instance3.__str__(), output_str)
+
+    def test_all_with_updated_instance(self):
+        # Create an instance of City
+        instance = City()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all City")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+
+        # Update the instance
+        instance.name = "Updated City"
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all City")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+    
+    def test_all_amenity(self):
+        # Create a few instances of Amenity
+        instance1 = Amenity()
+        instance2 = Amenity()
+        instance3 = Amenity()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all Amenity")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance1.__str__(), output_str)
+            self.assertIn(instance2.__str__(), output_str)
+            self.assertIn(instance3.__str__(), output_str)
+
+    def test_all_with_updated_instance(self):
+        # Create an instance of Amenity
+        instance = Amenity()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all Amenity")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+
+        # Update the instance
+        instance.name = "Updated Amenity"
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all Amenity")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance.__str__(), output_str)
+
+    def test_all_place(self):
+        # Create a few instances of Place
+        instance1 = Place()
+        instance2 = Place()
+        instance3 = Place()
+
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.console.onecmd("all Place")
+            output_str = output.getvalue().strip()
+            self.assertIn(instance1.__str__(), output_str)
+            self.assertIn(instance2.__str__(), output_str)
+            self.assertIn(instance3.__str__(), output_str)
+
+
 
 
 if __name__ == "__main__":
